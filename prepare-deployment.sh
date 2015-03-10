f871111a0728d4b797ea1a5333eb962001fef1e3
Daniel Naber <naber@danielnaber.de>
2015-03-10 22:13:16 +0100

    use real server name again, not the temporary one

--- prepare-deployment.sh
+++ prepare-deployment.sh
@@ -8,8 +8,8 @@ pushd
 
 ./cleancache.sh
 grails --offline war && \
-  scp target/languagetool-wikicheck-0.1.war languagetool@community2.languagetool.org:/tmp/wikicheck.war && \
-  ssh languagetool@community2.languagetool.org unzip -d /home/languagetool/tomcat/webapps/wikiCheck /tmp/wikicheck.war && \
+  scp target/languagetool-wikicheck-0.1.war languagetool@community.languagetool.org:/tmp/wikicheck.war && \
+  ssh languagetool@community.languagetool.org unzip -d /home/languagetool/tomcat/webapps/wikiCheck /tmp/wikicheck.war && \
   echo "Now call these commands to finish the deployment:" && \
   echo "  ssh languagetool@languagetool.org" && \
   echo "  ./restart-tomcat.sh"
